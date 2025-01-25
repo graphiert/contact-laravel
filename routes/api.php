6a49304a8a8a5f0f0b71c2eb7eddaa0ed08a21c8
graphiert <galihpujiirianto@gmail.com>
2025-01-25 17:02:37 +0700

    fix: set accept headers for api with application/json

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,10 @@ use App\Http\Controllers\Api\ContactController;
 use Illuminate\Support\Facades\Route;
 
 // AuthController
-Route::post('/token', [AuthController::class, 'token']);
-Route::post('/revoke', [AuthController::class, 'revoke'])->middleware('auth:sanctum');
+Route::post('/token', [AuthController::class, 'token'])
+    ->middleware('header.json');
+Route::post('/revoke', [AuthController::class, 'revoke'])
+    ->middleware(['header.json', 'auth:sanctum']);
 
-Route::apiResource('/contacts', ContactController::class)->middleware('auth:sanctum');
+Route::apiResource('/contacts', ContactController::class)
+    ->middleware(['header.json', 'auth:sanctum']);
